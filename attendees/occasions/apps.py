32c83bb21589e6b880268b1df743260c21b4f1d7
Jack Lin <jlin@castlighthealth.com>
2022-04-08 17:50:38 -0700

    track schedule Event model by PG history

--- attendees/occasions/apps.py
+++ attendees/occasions/apps.py
@@ -1,5 +1,15 @@
+import pghistory
 from django.apps import AppConfig
+from django.apps import apps as django_apps
 
 
 class OccasionsConfig(AppConfig):
     name = "attendees.occasions"
+
+    def ready(self):
+        schedule_event_model = django_apps.get_model("schedule.Event", require_ready=False)
+
+        pghistory.track(
+            pghistory.Snapshot('event.snapshot'),
+            app_label='occasions'
+        )(schedule_event_model)
