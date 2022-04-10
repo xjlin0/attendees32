1d5fb9804b33642aaa2b5aff32e9904dcfd2c482
Jack Lin <jlin@castlighthealth.com>
2022-04-10 06:02:09 -0700

    now tracking schedule's Calendar wth p history

--- attendees/occasions/apps.py
+++ attendees/occasions/apps.py
@@ -7,8 +7,17 @@ class OccasionsConfig(AppConfig):
     name = "attendees.occasions"
 
     def ready(self):
+        schedule_calendar_model = django_apps.get_model("schedule.Calendar", require_ready=False)
+        # schedule_calendarrelation_model = django_apps.get_model("schedule.CalendarRelation", require_ready=False)
         schedule_event_model = django_apps.get_model("schedule.Event", require_ready=False)
         schedule_eventrelation_model = django_apps.get_model("schedule.EventRelation", require_ready=False)
+
+        pghistory.track(
+            pghistory.Snapshot('calendar.snapshot'),
+            related_name='history',
+            model_name='CalendarHistory',
+            app_label='occasions'
+        )(schedule_calendar_model)
 
         pghistory.track(
             pghistory.Snapshot('event.snapshot'),
