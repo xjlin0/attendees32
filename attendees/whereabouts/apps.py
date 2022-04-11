cce139209fce84964ddaed95ada831fa4716b7e2
Jack Lin <jlin@castlighthealth.com>
2022-04-11 11:42:10 -0700

    now tracked state by pghistory

--- attendees/whereabouts/apps.py
+++ attendees/whereabouts/apps.py
@@ -8,6 +8,7 @@ class WhereaboutsConfig(AppConfig):
 
     def ready(self):
         address_country_model = django_apps.get_model("address.Country", require_ready=False)
+        address_state_model = django_apps.get_model("address.State", require_ready=False)
 
         pghistory.track(
             pghistory.Snapshot('country.snapshot'),
@@ -15,3 +16,10 @@ class WhereaboutsConfig(AppConfig):
             model_name='CountryHistory',
             app_label='whereabouts'
         )(address_country_model)
+
+        pghistory.track(
+            pghistory.Snapshot('state.snapshot'),
+            related_name='history',
+            model_name='StateHistory',
+            app_label='whereabouts'
+        )(address_state_model)
