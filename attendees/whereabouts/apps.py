7033a82cb41e7b368d518dcab228339ee6ec5802
Jack Lin <jlin@castlighthealth.com>
2022-04-11 11:31:55 -0700

    tracked address country by pghistory

--- attendees/whereabouts/apps.py
+++ attendees/whereabouts/apps.py
@@ -1,5 +1,17 @@
+import pghistory
 from django.apps import AppConfig
+from django.apps import apps as django_apps
 
 
 class WhereaboutsConfig(AppConfig):
     name = "attendees.whereabouts"
+
+    def ready(self):
+        address_country_model = django_apps.get_model("address.Country", require_ready=False)
+
+        pghistory.track(
+            pghistory.Snapshot('country.snapshot'),
+            related_name='history',
+            model_name='CountryHistory',
+            app_label='whereabouts'
+        )(address_country_model)
