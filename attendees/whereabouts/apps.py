37fbca9847fb2e441647a5aca38e0ed9fe7406b2
Jack Lin <jlin@castlighthealth.com>
2022-04-11 11:48:29 -0700

    tracked locality history by pghistory now

--- attendees/whereabouts/apps.py
+++ attendees/whereabouts/apps.py
@@ -9,6 +9,7 @@ class WhereaboutsConfig(AppConfig):
     def ready(self):
         address_country_model = django_apps.get_model("address.Country", require_ready=False)
         address_state_model = django_apps.get_model("address.State", require_ready=False)
+        address_locality_model = django_apps.get_model("address.Locality", require_ready=False)
 
         pghistory.track(
             pghistory.Snapshot('country.snapshot'),
@@ -23,3 +24,10 @@ class WhereaboutsConfig(AppConfig):
             model_name='StateHistory',
             app_label='whereabouts'
         )(address_state_model)
+
+        pghistory.track(
+            pghistory.Snapshot('locality.snapshot'),
+            related_name='history',
+            model_name='LocalityHistory',
+            app_label='whereabouts'
+        )(address_locality_model)
