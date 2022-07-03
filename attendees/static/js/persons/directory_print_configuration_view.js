dd08fa5855807c8d0bd7872b73d3736f7b9c056b
Jack Lin <jlin@castlighthealth.com>
2022-07-02 21:38:59 -0700

    get pdf configuration page showing

---
+++ attendees/static/js/persons/directory_print_configuration_view.js
@@ -0,0 +1,11 @@
+Attendees.directoryPrintConfiguration = {
+
+  init: () => {
+    console.log('static/js/persons/directory_print_configuration_view.js');
+  },
+
+};
+
+$(document).ready(() => {
+  Attendees.directoryPrintConfiguration.init();
+});
