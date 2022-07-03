9334d0b61c563f6c22f21f46759c97b596a84b7c
Jack Lin <jlin@castlighthealth.com>
2022-07-02 22:09:17 -0700

    add form input name to get values

--- attendees/static/js/persons/directory_print_configuration_view.js
+++ attendees/static/js/persons/directory_print_configuration_view.js
@@ -2,6 +2,22 @@ Attendees.directoryPrintConfiguration = {
 
   init: () => {
     console.log('static/js/persons/directory_print_configuration_view.js');
+    Attendees.directoryPrintConfiguration.catchSubmitToGeneratePDF();
+  },
+
+  catchSubmitToGeneratePDF: () => {
+    const configurationForm = document.getElementById("directory-print-configuration");
+    configurationForm.addEventListener("submit", (event) => {
+
+      event.preventDefault();
+
+
+      const formData = new FormData(event.target);
+      const formProps = Object.fromEntries(formData);
+      console.log('clicked! here is formProps');
+      console.log(formProps);
+      //document.directoryPrintConfiguration['directory-header'].value;
+    });
   },
 
 };
