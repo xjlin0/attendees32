df3e65a6271c3aca492351d18f6d27f43010e5c1
Jack Lin <jlin@castlighthealth.com>
2022-08-20 06:56:26 -0700

    register timezone middleware so that attendance start/finish can be shown in client's timezone

--- attendees/middleware.py
+++ attendees/middleware.py
@@ -1,13 +1,21 @@
 import pytz
 from urllib import parse
 from django.utils import timezone
-from django.utils.deprecation import MiddlewareMixin
+# from django.utils.deprecation import MiddlewareMixin
 from django.conf import settings
 
 
-class TimezoneMiddleware(MiddlewareMixin):
+class TimezoneMiddleware:
 
-    @staticmethod
-    def process_request(request):
+    # @staticmethod
+    # def process_request(request):
+    #     tzname = request.COOKIES.get('timezone') or settings.CLIENT_DEFAULT_TIME_ZONE
+    #     timezone.activate(pytz.timezone(parse.unquote(tzname)))
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
         tzname = request.COOKIES.get('timezone') or settings.CLIENT_DEFAULT_TIME_ZONE
         timezone.activate(pytz.timezone(parse.unquote(tzname)))
+        return self.get_response(request)
