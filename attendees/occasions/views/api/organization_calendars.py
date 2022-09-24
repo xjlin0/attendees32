696466a1720b698c51d042aa4dd1554b8235fda9
Jack Lin <jlin@castlighthealth.com>
2022-09-24 11:23:41 -0700

    draft of callendar endpopint

--- attendees/occasions/views/api/organization_calendars.py
+++ attendees/occasions/views/api/organization_calendars.py
@@ -1,71 +1,60 @@
 import time
 
 from django.contrib.auth.decorators import login_required
+from django.contrib.contenttypes.models import ContentType
 from django.db.models import F, Q
 from django.utils.decorators import method_decorator
 from rest_framework import viewsets
 from rest_framework.exceptions import AuthenticationFailed
-from rest_framework.response import Response
+from schedule.models import Calendar
 
-from attendees.occasions.models import Character
-from attendees.occasions.serializers import CharacterAssemblySerializer
-from attendees.persons.models import Utility
+from attendees.occasions.serializers import CalendarListSerializer
 
 
 @method_decorator([login_required], name="dispatch")
-class OrganizationCharactersViewSet(viewsets.ModelViewSet):
+class OrganizationCalendarsViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows all Meet in current user's organization filtered by date to be viewed or edited.
-    Todo 20210711 only coworkers/organizers can see all Characters, general users should only see what they attended
-    # Todo 20210815 if limiting by meet's shown_audience, non-coworker assigned to non-public meets won't show
+    API endpoint that may expose all calendars in current users organization filtered by users roles.
     """
 
-    serializer_class = CharacterAssemblySerializer
-
-    def list(self, request, *args, **kwargs):
-        grouping = request.query_params.get("grouping")
-        queryset = self.filter_queryset(self.get_queryset())
-        page = self.paginate_queryset(queryset)
-
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(
-                Utility.transform_result(serializer.data, grouping)
-            )
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(Utility.transform_result(serializer.data, grouping))
+    serializer_class = CalendarListSerializer
 
     def get_queryset(self):
-        current_user_organization = self.request.user.organization
-        search_value = self.request.query_params.get("searchValue")
-        search_expression = self.request.query_params.get("searchExpr")
-        search_operation = self.request.query_params.get("searchOperation")
-        assemblies = self.request.query_params.getlist("assemblies[]")
-        if current_user_organization:
+        user_organization = self.request.user.organization
+        if user_organization:
+            distinction = self.request.query_params.get("distinction", "source")
+            search_value = self.request.query_params.get("searchValue")
+            search_expression = self.request.query_params.get("searchExpr")
+            search_operation = self.request.query_params.get("searchOperation")
+            organization_acronym = user_organization.infos.get('acronym').strip()
+            extra_filter = Q(slug__istartswith=organization_acronym)
+            extra_filter.add(Q(calendarrelation__distinction=distinction), Q.AND)
 
-            extra_filter = Q(assembly__division__organization=current_user_organization)
+            if search_value and search_expression == 'display_name' and search_operation == 'contains':
+                extra_filter.add(Q(display_name__icontains=search_value), Q.AND)
 
-            if assemblies:
-                extra_filter.add(Q(assembly__in=assemblies), Q.AND)
+            if not self.request.user.can_see_all_organizational_meets_attendees():
+                extra_filter.add(
+                    (Q(calendarrelation__content_type=ContentType.objects.filter(model='organization').first().id)
+                     &
+                     Q(calendarrelation__object_id=user_organization.id
+                       )
+                     ), Q.OR)
 
-            if search_value and search_operation == 'contains':  # only contains supported now
-                terms = {f'{search_expression}__icontains': search_value}
-                extra_filter.add(Q(**terms), Q.AND)
+                extra_filter.add(
+                    (Q(calendarrelation__content_type=ContentType.objects.filter(model='user').first().id)
+                     &
+                     Q(calendarrelation__object_id=self.request.user.id
+                       )
+                     ), Q.OR)
 
-            return (
-                Character.objects.filter(extra_filter)
-                .annotate(
-                    assembly_name=F("assembly__display_name"),
-                )
-                .order_by("assembly_name")
-            )
+            return Calendar.objects.filter(extra_filter)
 
         else:
             time.sleep(2)
             raise AuthenticationFailed(
-                detail="Have you registered any events of the organization?"
+                detail="Has your account assigned an organization?"
             )
 
 
-organization_characters_viewset = OrganizationCharactersViewSet
+organization_calendars_viewset = OrganizationCalendarsViewSet
