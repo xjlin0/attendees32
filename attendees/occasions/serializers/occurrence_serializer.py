83a5aa71496686dc014d21096bee276d806e36d8
Jack Lin <jlin@castlighthealth.com>
2022-09-30 21:03:46 -0700

    add US holidays

--- attendees/occasions/serializers/occurrence_serializer.py
+++ attendees/occasions/serializers/occurrence_serializer.py
@@ -8,7 +8,7 @@ class OccurrenceSerializer(serializers.ModelSerializer):
     text = serializers.SerializerMethodField(read_only=True)
 
     def get_text(self, obj):
-        return f"{Utility.get_object_name(obj, 'title', 'meet_display_name')} {Utility.get_object_name(obj, 'description', 'display_name')}"
+        return f"{Utility.get_object_name(obj, 'title', 'meet_display_name') or ''} {Utility.get_object_name(obj, 'description', 'display_name') or ''}".strip() or obj.title or obj.description
 
     class Meta:
         model = Occurrence
