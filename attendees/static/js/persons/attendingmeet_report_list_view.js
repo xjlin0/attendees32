3bfdccc868ac0dfad57a623d810d687f42449496
Jack Lin <jlin@castlighthealth.com>
2023-05-31 11:26:59 -0700

    finally got page ready equivelent in JS

--- attendees/static/js/persons/attendingmeet_report_list_view.js
+++ attendees/static/js/persons/attendingmeet_report_list_view.js
@@ -6,10 +6,32 @@ window.Attendees = {
 
     init: () => {
       console.log('static/js/persons/attendingmeet_report_list_view.js');
+      Attendees.attendingmeetReportListView.startListener();
+    },
+
+    startListener: () => {
+      const mainDiv = document.querySelector('div.attendingmeet-report-container');
+      if (mainDiv) {
+        mainDiv.addEventListener('click', (event) => {
+        console.log('clicked  and here is the id: ', event.target.id);
+          if (event.target.matches('div.member') && event.target.id) {
+              console.log('clicked!');
+          }
+        }, false);
+
+      } else {
+        console.log('report does not exits! ');
+      }
+    },
+
+    mustRunLast: () => {  // https://stackoverflow.com/a/74956907/4257237
+      if (!document.querySelector('div.attendingmeet-report-container')) {
+        setTimeout(Attendees.attendingmeetReportListView.mustRunLast, 100);
+        return;
+      }
+      Attendees.attendingmeetReportListView.init();
     },
   }
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-   Attendees.attendingmeetReportListView.init();
-}, false);
+document.addEventListener('load', Attendees.attendingmeetReportListView.mustRunLast());
